7aef2abd8c5fd5e29aef18f755120d999c2e3d4e
zoneslee <zoneslee@muchenglin.com>
2024-12-24 14:50:41 +0800

    feat:1.Dockerfile

--- RebuildDocker.sh
+++ RebuildDocker.sh
@@ -5,7 +5,7 @@ sudo docker rmi zoneslee/dockergoproject:v1.0
 
 # 打包成二进制可执行文件
 go mod tidy
-go build -o DockerGoProjectAoServer.exe ./DockerGoProjectAo/server/server.go
+go build -o DockerGoProjectAoServer ./DockerGoProjectAo/server/server.go
 
 
 sudo docker build -t zoneslee/dockergoproject:v1.0 .
