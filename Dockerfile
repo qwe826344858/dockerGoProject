437a9b620c68dcf41751af43ead751e392df51fc
zoneslee <zoneslee@muchenglin.com>
2024-12-24 14:49:21 +0800

    feat:1.Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# 使用 golang 作为基础镜像
+FROM golang:1.23.4
+
+# 设置工作目录
+WORKDIR /usr/src/app/dockerGoProject
+
+# 复制Ao服务到容器中
+COPY DockerGoProjectAoServer.exe /usr/src/app/dockerGoProject
+
+ENV GOPROXY=https://goproxy.cn,direct
+ENV GOPRIVATE=.gitlab.com,.gite.com
+
+# 运行 main.py
+# CMD ["go","run", "/usr/src/app/dockerGoProject/DockerGoProjectAo/server/server.go",">>","/usr/src/app/dockerGoProject/std_out.log","2>&1","&"]
+CMD ["./DockerGoProjectAoServer",">>","std_out.log","2>&1"]
+
---
+++ RebuildDocker.sh
@@ -0,0 +1,16 @@
+# shell
+
+sudo docker rm dockergoprojectao
+sudo docker rmi zoneslee/dockergoproject:v1.0
+
+# 打包成二进制可执行文件
+go mod tidy
+go build -o DockerGoProjectAoServer.exe ./DockerGoProjectAo/server/server.go
+
+
+sudo docker build -t zoneslee/dockergoproject:v1.0 .
+
+#sudo docker run --name dockergoprojectao -p 40001:40001 zoneslee/dockergoproject:v1.0 &
+sudo docker run --name dockergoprojectao -p 40001:40001 zoneslee/dockergoproject:v1.0
+# 进入容器内部
+# sudo docker run  -it --name dockergoprojectao zoneslee/dockergoproject:v1.0 /bin/bash
